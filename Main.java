1835d9495e4954b7303b6a31950f25e87bb4a97a
Kashvi Singh <127826843+scitech-butterfly@users.noreply.github.com>
2025-04-18 16:11:46 +0530

    Created instances of each thread

--- Main.java
+++ Main.java
@@ -5,5 +5,10 @@
 // Main.java
 
 public class Main {
+    public static void main(String[] args) {
+        // Creating instances of each thread class
+        EvenThread evenThread = new EvenThread();
+        OddThread oddThread = new OddThread();
+        CharThread charThread = new CharThread();
   
 }
